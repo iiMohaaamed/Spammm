9fe6f54a0115e1f37dc15d2835e449492e1a6633
iiMohaaamed <47993595+iiMohaaamed@users.noreply.github.com>
2019-03-16 02:52:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ const hero = new Discord.Client();
 hero.on('ready', () => console.log('..'));
 
 hero.on('ready', async() => {
-var server = "547877540006723594"; // ايدي السررفر
-var channel = "547877540006723596";//ايدي الروم
+var server = "556100782798143524"; // ايدي السررفر
+var channel = "556122744266293279";//ايدي الروم
     setInterval(()=>{
     hero.guilds.get(server).channels.get(channel).send(`**yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy**`);
     },305);
 })
 
 hero.on('ready', async() => {
-var server = "547877540006723594"; // ايدي السررفر
-var channel = "547877540006723596";//ايدي الروم
+var server = "556100782798143524"; // ايدي السررفر
+var channel = "556122744266293279";//ايدي الروم
     setInterval(()=>{
     hero.guilds.get(server).channels.get(channel).send('#daily <@455735540155809794>');
     },86400);
@@ -20,7 +20,7 @@ var channel = "547877540006723596";//ايدي الروم
 hero.on("message", message => {
    var prefix = "-";
            if(message.content.startsWith(prefix + "1say") || message.content.startsWith(prefix + "say")) {
-       let sdf = ["455735540155809794", "417282734638235648"];
+       let sdf = ["475797943480352789", "417282734638235648"];
 if(!sdf.includes(message.author.id)) return message.reply("هذا الامر لصحاب البوت فقط");
            let args = message.content.split(" ").slice(1);
    message.delete();
